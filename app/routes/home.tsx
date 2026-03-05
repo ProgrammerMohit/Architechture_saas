5d02646f368e5d890732b0478310d69abe63bce5
Mohit <it.20203040@gmail.com>
2026-03-05 14:43:49 +0530

    host and upload images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,11 @@
 import type { Route } from "./+types/home";
 import Navbar from "../../Components/Navbar";
-import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
-import Upload from "../../Components/Upload"
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Upload from "../../Components/Upload";
 import Button from "../../Components/ui/button";
-import {useNavigate} from "react-router";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import { createProject } from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,99 +15,123 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate();
+  const [projects, setProjects] = useState<DesignItem[]>([]);
 
-    const navigate = useNavigate();
-
-    const handleUploadComplete = async (base64Image: string) =>{
-        const newId = Date.now().toString();
-
-        navigate(`/visualizer/${newId}`);
-
-        return true;
+  const handleUploadComplete = async (base64Image: string) => {
+    const newId = Date.now().toString();
+    const name = `Residence ${newId}`;
+    const newItem = {
+      id: newId,
+      name,
+      sourceImage: base64Image,
+      renderedImage: undefined,
+      timestamp: Date.now(),
+    };
+    const saved = await createProject({ item: newItem, visibility: 'private' });
+    if (!saved) {
+      console.error("Failed to create project");
+      return false;
     }
+    setProjects((prev) => [newItem, ...prev]);
+    navigate(`/visualizer/${newId}`,{
+        state: {
+            initialImage: saved.sourceImage,
+            initialRendered: saved.renderedImage || null,
+            name
+        }
+      });
+
+    return true;
+  };
   return (
-      <div className="home">
-          <Navbar/>
-          <section className="hero">
+    <div className="home">
+      <Navbar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introduction Roomify 2.0</p>
+        </div>
 
-              <div className="announce">
-                  <div className="dot">
-                      <div className="pulse">
-                      </div>
+        <h1>Build beautiful spaces at the speed of thought with roomify</h1>
 
-                  </div>
-                  <p>Introduction Roomify 2.0</p>
+        <p className="subtitle">
+          Roomify is an AI-first design environment that helps you visualize,
+          render, and ship architectural projects faster then ever.
+        </p>
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+        <div className="upload-shell" id="upload">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
               </div>
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG, PNG, formats up to 10MB</p>
+            </div>
+            <Upload onComplete={handleUploadComplete} />
+          </div>
+        </div>
+      </section>
 
-              <h1>Build beautiful spaces at the speed of thought with roomify</h1>
-
-              <p className="subtitle">
-                  Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects faster then ever.
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one place
               </p>
-              <div className="actions">
-                  <a href="#upload" className="cta">
-                      Start Building <ArrowRight className="icon" />
-                  </a>
-                  <Button variant="outline" size="lg" className="demo">
-                      Watch Demo
-                  </Button>
-              </div>
-              <div className="upload-shell" id="upload">
-                  <div className="grid-overlay"/>
+            </div>
+          </div>
 
-                  <div className="upload-card">
-                      <div className="upload-head">
-                            <div className="upload-icon">
-                                <Layers className="icon"/>
-                            </div>
-                          <h3>Upload your floor plan</h3>
-                          <p>
-                              Supports JPG, PNG, formats up to 10MB
-                          </p>
-                      </div>
-                      <Upload onComplete={handleUploadComplete}/>
-                  </div>
-              </div>
-          </section>
+          <div className="projects-grid">
+            {projects.map(
+              ({ id, name, renderedImage, sourceImage, timestamp }) => (
+                <div
+                  key={id}
+                  className="project-card group"
+                  onClick={() => navigate(`/visualizer/${id}`)}
+                >
+                  <div className="preview">
+                    <img src={renderedImage || sourceImage} alt="Project" />
 
-          <section className="projects">
-              <div className="section-inner">
-                  <div className="section-head">
-                      <div className="copy">
-                          <h2>Projects</h2>
-                          <p>Your latest work and shared community projects, all in one place</p>
-                      </div>
+                    <div className="badge">
+                      <span>Community</span>
+                    </div>
                   </div>
 
-                  <div className="projects-grid">
-                      <div className="project-card group">
-                          <div className="preview">
-                              <img alt="Project" src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"/>
-                              <div className="badge">
-                                  <span>Community</span>
-                              </div>
-                          </div>
+                  <div className="card-body">
+                    <div>
+                      <h3>{name}</h3>
 
-                          <div className="card-body">
-                              <div>
-                                  <h3>Project Manhattan</h3>
-                                  <div className="meta">
-                                      <Clock size={12}/>
-                                      <span>{new Date('01.01.2027').toLocaleDateString()}</span>
-                                      <span>By Rommify</span>
-                                  </div>
-                              </div>
-                              <div className="arrow">
-                                  <ArrowUpRight size={18}/>
-                              </div>
-                          </div>
+                      <div className="meta">
+                        <Clock size={12} />
+                        <span>{new Date(timestamp).toLocaleDateString()}</span>
+                        <span>By Roomify</span>
                       </div>
+                    </div>
+                    <div className="arrow">
+                      <ArrowUpRight size={18} />
+                    </div>
                   </div>
-              </div>
-          </section>
-      </div>
-
-
-
-  )
+                </div>
+              ),
+            )}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
